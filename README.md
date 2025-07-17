d8a3f8e11259db5edcf100c22cd17cb126280ba6
huynhhuuthangdev <huynhhuuthang@devuit.onmicrosoft.com>
2025-07-17 10:36:52 +0700

    update readme

---
+++ README.md
@@ -0,0 +1,8 @@
+for local running create a tfvars file that contain these require secrets:
+ARM_SUBSCRIPTION_ID = 
+ARM_TENANT_ID = 
+ARM_CLIENT_ID = 
+ARM_CLIENT_SECRET = 
+
+//// Agent VM Credentials
+admin_password = "Admin@123456"
--- modules/virtual_machine/main.tf
+++ modules/virtual_machine/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_linux_virtual_machine" "agent_vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "22_04-lts" 
     version   = "latest"
   }
   tags = {
